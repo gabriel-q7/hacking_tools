9fa0362203bd609046c072b84c49fed212d41253
gabriel <gabriel_aq7@hotmail.com>
2022-10-18 08:38:08 -0300

    upds

--- Recon/discovery.py
+++ Recon/discovery.py
@@ -1,12 +1,19 @@
 import requests
+import urllib3
+import pyfiglet
+
+banner = pyfiglet.figlet_format("-=D1SC0V3RY=-")
+print(banner)
 
 site = input('Site: ')
 wordList = input('WordList(abs path): ')
 
 file = open(wordList)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 for d in file.readlines():
-   request_ = requests.get(site + '/' + d.replace('\n',''))
+   request_ = requests.get(site + '/' + d.replace('\n',''), verify = False)
 
    if str(request_.status_code) != "404":
       print(request_.url + "status=>" + str(request_.status_code))
